103940a92ca87ff1af1ccc4869fc51538ca7a7f1
Pulsar87 <shaheen.mhd.tareq@gmail.com>
2026-05-24 14:04:12 -0400

    fixed admin

--- users/models.py
+++ users/models.py
@@ -6,7 +6,9 @@ from django.db import models
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    tenant = models.ForeignKey("tenants.Tenant", on_delete=models.CASCADE)
+    tenant = models.ForeignKey(
+        "tenants.Tenant", on_delete=models.CASCADE, null=True, blank=True
+    )
     facility = models.ForeignKey(
         "tenants.Facility", on_delete=models.SET_NULL, null=True, blank=True
     )
